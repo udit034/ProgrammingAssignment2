34cb6b2d7e5617c55ada66b217841082add65dde
udit034 <udit.mehata@gmail.com>
2014-09-19 22:56:00 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix calculations can be expensive to compute and hence, these functions try to store the results of matrix mean,
+## and the original matrix in the cache.
+## Thus , when these parameters are set, user can be able to get them back from the cache by using the get functions.
+## This would save computational cost.
 
-## Write a short comment describing this function
+
+
+## MakeCacheMatrix function accepts matrix arguments and returns a list of functions that can be used to 
+## a) SET the value of the matrix
+## b) GET the value of the matrix
+## c) SET the mean value of the matrix the special Matrix object created.
+## d) GET the mean value of the special matrix object from the cache.
+
+## the function returns a list of functions as described above.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y = matrix()) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 ## Write a short comment describing this function
